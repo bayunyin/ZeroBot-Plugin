34d4a292472bba1dc3799710ce116fb66937219c
bayunyin <112416713+bayunyin@users.noreply.github.com>
2022-08-30 00:39:03 +0800

    123

--- main.go
+++ main.go
@@ -204,8 +204,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	 sus = append(sus, 2564853559)
+	
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
